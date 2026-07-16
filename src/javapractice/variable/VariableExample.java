a8cd9b059699a93ac84e8314578fc7f011e6511f
loi2016 <loi2012@naver.com>
2026-07-16 23:46:26 +0900

    chore: add automatic study PR workflow

---
+++ src/javapractice/variable/VariableExample.java
@@ -0,0 +1,57 @@
+package javapractice.variable;
+
+public class VariableExample {
+
+	public static void main(String[] args) {
+
+		// =====================
+		// 1. 변수 선언 및 초기화
+		// =====================
+
+		int age = 20;
+		String name = "GilDong";
+
+		System.out.println(age); // 예상 결과 : 20
+		System.out.println(name); // 예상 결과 : GilDong
+
+		// =====================
+		// 2. 값 변경
+		// =====================
+
+		age = 25;
+
+		System.out.println(age); // 예상 결과 : 25
+
+		// =====================
+		// 3. 추가 자료형
+		// =====================
+
+		boolean isDeveloper = true;
+		double height = 180.3;
+		char gender = 'M';
+
+		System.out.println(isDeveloper); // 예상 결과 : true
+		System.out.println(height); // 예상 결과 : 180.3
+		System.out.println(gender); // 예상 결과 : M
+
+		// =====================
+		// 4. 연산
+		// =====================
+		int a = 3;
+		int b = 5;
+
+		int sum = a + b;
+		int product = a * b;
+
+		System.out.println(sum); // 예상 결과 : 8
+		System.out.println(product); // 예상 결과 : 15
+
+		// =====================
+		// 5. String 합성
+		// =====================
+
+		String message = name + "의 올해 나이는 " + age + "살";
+
+		System.out.println(message); // 예상 결과 : GilDong의 올해 나이는 25살
+	}
+}
